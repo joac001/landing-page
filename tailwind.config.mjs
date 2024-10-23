7247d45ab7489219d29eb8e58976a2b929426bb8
joac001 <jordonez@fi.uba.ar>
2024-10-23 17:04:30 -0300

    Refactor Card component and add Footer and Header components
    - Refactor the Card component in src/components/Card.astro to use the updated props: image, title, description, and stl.
    - Add the Footer component in src/components/Footer.astro.
    - Add the Header component in src/components/Header.astro.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,13 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
-	},
-	plugins: [],
+		extend: {
+			colors: {
+				yellow: { 50: '#fcc406' },
+				blue: { 50: '#010132' },
+				gray: { 50: '#757575' }
+			},
+		},
+		plugins: [],
+	}
 }
